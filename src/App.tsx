fa766acc842c793e3af16e421aebcbd362cb6b70
Peto.B000 <peto.b000@gmail.com>
2025-01-13 13:31:18 +0100

    deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,10 @@ const App: FC = () => {
         <Header />
         <div className="container-fluid">
           <Routes>
-            <Route path="/job_test_FE/" element={<Navigate to="/category/1" replace />} />
-            <Route path="/category/:categoryId" element={<CategoryPage />} />
-            <Route path="/product/:productId" element={<ProductPage />} />
+            <Route path="/" element={<Navigate to="category/1" replace />} />
+            <Route path="category/:categoryId" element={<CategoryPage />} />
+            <Route path="product/:productId" element={<ProductPage />} />
           </Routes>
-          
         </div>
       </CartProvider>
     </HashRouter>
