839c9d603ba8dfdb5d6ce51b5c40dffea0455339
Peto.B000 <peto.b000@gmail.com>
2025-01-13 19:13:23 +0100

    added changes required for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/job_test_FE/',
+  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
+    assetsDir: 'assets',
+    sourcemap: true
   }
 })
