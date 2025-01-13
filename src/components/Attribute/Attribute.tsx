b82596c4b550dacb3324d652383fc631fb44416d
Peto.B000 <peto.b000@gmail.com>
2025-01-13 22:09:39 +0100

    attribute testid fix

--- src/components/Attribute/Attribute.tsx
+++ src/components/Attribute/Attribute.tsx
@@ -38,7 +38,7 @@ const Attribute: FC<AttributeProps> = ({ attribute, selectedValue, onSelect, var
         {attribute.items.map((item) => (
           <div
             key={item.id}
-            data-testid={variant === 'product' ? `product-attribute-${attribute.name.toLowerCase()}-${item.value.toLowerCase()}` : ""}
+            data-testid={variant === 'product' ? `product-attribute-${attribute.name.toLowerCase()}-${item.value.toLowerCase()}` : getCartItemTestId(item.displayValue, selectedValue === item.id)}
             className={variant === 'product' ? "product-attribute-item me-3" : "col p-0"}
             onClick={() => onSelect && onSelect(attribute.id, item.id)}
             style={{ cursor: onSelect ? 'pointer' : 'default' }}
@@ -50,7 +50,6 @@ const Attribute: FC<AttributeProps> = ({ attribute, selectedValue, onSelect, var
                   ? { backgroundColor: item.displayValue }
                   : undefined
               }
-              data-testid={variant === 'cart' ? getCartItemTestId(item.displayValue, selectedValue === item.id) : undefined}
             >
               {attribute.type !== "swatch" && item.displayValue}
             </div>
